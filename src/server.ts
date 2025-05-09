7a8b50605f129b077a4524961efc39c7f999c2dc
oladapo <oladapoelijah49@gmail.com>
2025-05-09 08:34:50 +0100

    set authorization token in the headers to the auth microservices

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ import { Application, NextFunction, Request, Response, json, urlencoded } from '
 import { StatusCodes } from 'http-status-codes';
 import { elasticSearch } from '@gateway/elasticsearch';
 import { appRoutes } from '@gateway/routes';
+import { axiosAuthInstance } from '@gateway/services/api/auth.service';
 
 const SERVER_PORT = 4000;
 const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'apiGatewayServer', 'debug');
@@ -55,7 +56,7 @@ export class gatewayServer {
 
     app.use((req: Request, _res: Response, next: NextFunction) => {
       if (req.session?.jwt) {
-        // axiosAuthInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        axiosAuthInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
         // axiosBuyerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
         // axiosSellerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
         // axiosGigInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
