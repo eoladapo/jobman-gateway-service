e46ccb2bdb9d213d8f3639eecf77036578f33694
oladapo <oladapoelijah49@gmail.com>
2025-05-14 16:38:20 +0100

    Implement the buyer and seller service

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,8 @@ import { StatusCodes } from 'http-status-codes';
 import { checkConnection } from '@gateway/elasticsearch';
 import { appRoutes } from '@gateway/routes';
 import { axiosAuthInstance } from '@gateway/services/api/auth.service';
+import { axiosBuyerInstance } from '@gateway/services/api/buyer.service';
+import { axiosSellerInstance } from '@gateway/services/api/seller.service';
 
 const SERVER_PORT = 4000;
 const log: Logger = winstonLogger(`${config.ELASTIC_SEARCH_URL}`, 'apiGatewayServer', 'debug');
@@ -57,8 +59,8 @@ export class GatewayServer {
     app.use((req: Request, _res: Response, next: NextFunction) => {
       if (req.session?.jwt) {
         axiosAuthInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
-        // axiosBuyerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
-        // axiosSellerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        axiosBuyerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
+        axiosSellerInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
         // axiosGigInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
         // axiosMessageInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
         // axiosOrderInstance.defaults.headers['Authorization'] = `Bearer ${req.session?.jwt}`;
